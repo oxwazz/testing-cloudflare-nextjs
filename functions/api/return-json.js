6cb9d8aede47ee618887e4353e794e2d78284f37
oxwazz <muhammad.rahmahalim@gmail.com>
2022-09-12 15:06:46 +0700

    update 20

--- functions/api/return-json.js
+++ functions/api/return-json.js
@@ -8,6 +8,15 @@ import {TES} from "../../halooo/sdfsdfsdf";
 // Reacts to POST /hello-worldh
 export async function onRequestPost({request}) {
   const datas = await request.json()
-  // const {data} = await axios.get(`https://jsonplaceholder.typicode.com/users/${datas.id ? datas.id : ""}`)
-  return Response.json({...datas,TES})
+  await fetch(`https://jsonplaceholder.typicode.com/users/${datas.id ? datas.id : ""}`, {
+    method: 'GET',
+    // headers: {
+    //   'Accept': 'application/json',
+    //   'Content-Type': 'application/json'
+    // },
+  });
+  const content = await rawResponse.json();
+  const {data} = await rawResponse.json();
+
+  return Response.json({...datas,TES,data})
 }
