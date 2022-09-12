4a0e363b58ae583e0a179b3d834f55f0f5224193
oxwazz <muhammad.rahmahalim@gmail.com>
2022-09-12 12:47:25 +0700

    update 1

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   swcMinify: true,
   images: {
     unoptimized: true,
   },
+
 };
 
 module.exports = nextConfig;
