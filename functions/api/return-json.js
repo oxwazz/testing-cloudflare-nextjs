e3b68b7974d2995b47175f35488c6a14fc639806
oxwazz <muhammad.rahmahalim@gmail.com>
2022-09-12 14:57:21 +0700

    update 17

--- functions/api/return-json.js
+++ functions/api/return-json.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from 'axios'
-import {TES} from "../../halooo/sdfsdfsdf";
+// import {TES} from "../../halooo/sdfsdfsdf";
 // export default function handler(req, res) {
 //   res.status(200).json({ name: 'John Doe' })
 // }
@@ -9,5 +9,5 @@ import {TES} from "../../halooo/sdfsdfsdf";
 export async function onRequestPost({request}) {
   const datas = await request.json()
   const {data} = await axios.get(`https://jsonplaceholder.typicode.com/users/${datas.id ? datas.id : ""}`)
-  return Response.json({...datas,...data, TES})
+  return Response.json({...datas,...data})
 }
