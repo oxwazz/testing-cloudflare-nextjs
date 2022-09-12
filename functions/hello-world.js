11518cb3ce48dc3387418a5e406f27fa97d477ba
oxwazz <muhammad.rahmahalim@gmail.com>
2022-09-12 14:14:26 +0700

    update 13

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,7 +1,7 @@
 // Reacts to POST /hello-world
 
-export async function onRequestPost(request) {
+export async function onRequestPost({request}) {
   // ...
-  return new Response(await request.json());
+  return new Response(JSON.stringify(await request.json()));
 
 }
