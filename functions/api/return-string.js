9e28bda9efa734f1c38be9a89e350853086d53d0
oxwazz <muhammad.rahmahalim@gmail.com>
2022-09-12 14:03:43 +0700

    update 10

--- functions/api/return-string.js
+++ functions/api/return-string.js
@@ -6,9 +6,5 @@
 
 // Reacts to POST /hello-world
 export async function onRequestPost({request}) {
-  const formData = await request.formData();
-  const body = {};
-  for (const entry of formData.entries()) {
-    body[entry[0]] = entry[1];
-  }  return new Response(JSON.stringify(body))
+  return new Response(JSON.stringify(request.body))
 }
